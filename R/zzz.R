197fb6b4ac043f917ef13128f4ec319354a7f8b0
Andrey Samokhin <97097314+AndreySamokhin@users.noreply.github.com>
2026-02-10 18:27:52 +0300

    🌪 import code from private package (#1)

---
+++ R/zzz.R
@@ -0,0 +1,31 @@
+#==============================================================================#
+#' @importFrom reticulate py_require
+#==============================================================================#
+.onLoad <- function(libname, pkgname) {
+
+  the$py_requirements <- list(
+    python = c(op = ">=", ver = "3.12"),
+    packages = list(
+      numpy = list(c(op = ">=",  ver = "2"),
+                   c(op = "<",   ver = "3")),
+      rdkit = list(c(op = ">=",  ver = "2025.9.1"))
+    )
+  )
+  the$python_ready <- FALSE
+
+  py_packages <- character(0L)
+  for (py_pkg_name in names(the$py_requirements$packages)) {
+    for (i in seq_along(the$py_requirements$packages[[py_pkg_name]])) {
+      ver <- paste(the$py_requirements$packages[[py_pkg_name]][[i]],
+                   collapse = "")
+      py_packages[[length(py_packages) + 1L]] <- paste0(py_pkg_name, ver)
+    }
+  }
+  reticulate::py_require(
+    python_version = paste(the$py_requirements$python, collapse = ""),
+    packages = py_packages
+  )
+  # cat(paste(py_packages, collapse = "\n"))
+}
+
+
