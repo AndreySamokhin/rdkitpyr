b41eac1bb13207d690fd3abe11c5621b226f5857
Andrey Samokhin <andrey.s.samokhin@gmail.com>
2026-02-12 08:42:25 +0300

    ✅ update incorrect SMILES and InChI strings

--- tests/testthat/test-molecule_io.R
+++ tests/testthat/test-molecule_io.R
@@ -1,5 +1,5 @@
 test_that("ParseMolecules()", {
-  mols <- ParseMolecules(c("C_???", "C"))
+  mols <- ParseMolecules(c("C???", "C"))
   expect_true(is.null(mols[[1L]]))
   expect_true(inherits(mols[[2L]], "rdkit.Chem.rdchem.Mol"))
   expect_identical(
@@ -15,7 +15,7 @@ test_that("ConvertToSmiles(), convert InChI to SMILES", {
     test_compounds$smiles
   )
   expect_identical(ConvertToSmiles("InChI=1S/CH4/h1H4"), "C")
-  expect_identical(ConvertToSmiles("InChI=1S/CH4/h1H4_???"), NA_character_)
+  expect_identical(ConvertToSmiles("InChI=???"), NA_character_)
   expect_identical(ConvertToSmiles(NA_character_), NA_character_)
 })
 
@@ -42,13 +42,13 @@ test_that("ConvertToSmiles(), convert SMILES to SMILES", {
     c("[CH3][CH2][CH2][OH]", "[cH]1[cH][cH][cH][cH][cH]1")
   )
   expect_identical(ConvertToSmiles("C"), "C")
-  expect_identical(ConvertToSmiles("C_???"), NA_character_)
+  expect_identical(ConvertToSmiles("C???"), NA_character_)
 })
 
 
 test_that("ConvertToInchi(), convert InChI to InChI", {
   expect_identical(ConvertToInchi("InChI=1S/CH4/h1H4"), "InChI=1S/CH4/h1H4")
-  expect_identical(ConvertToInchi("InChI=1S/CH4/h1H4_???"), NA_character_)
+  expect_identical(ConvertToInchi("InChI=???"), NA_character_)
 })
 
 
@@ -58,7 +58,7 @@ test_that("ConvertToInchi(), convert SMILES to InChI", {
     test_compounds$inchi
   )
   expect_identical(ConvertToInchi("C"), "InChI=1S/CH4/h1H4")
-  expect_identical(ConvertToInchi("C_???"), NA_character_)
+  expect_identical(ConvertToInchi("C???"), NA_character_)
   expect_identical(ConvertToInchi(NA_character_), NA_character_)
 })
 
@@ -72,7 +72,7 @@ test_that("ConvertToInchikey(), convert InChI to InChIKey", {
     ConvertToInchikey("InChI=1S/CH4/h1H4"),
     "VNWKTOKETHGBQD-UHFFFAOYSA-N"
   )
-  expect_identical(ConvertToInchikey("InChI=1S/CH4/h1H4_???"), NA_character_)
+  expect_identical(ConvertToInchikey("InChI=???"), NA_character_)
   expect_identical(ConvertToInchikey(NA_character_), NA_character_)
 })
 
@@ -83,7 +83,7 @@ test_that("ConvertToInchikey(), convert SMILES to InChIKey", {
     test_compounds$inchikey
   )
   expect_identical(ConvertToInchikey("C"), "VNWKTOKETHGBQD-UHFFFAOYSA-N")
-  expect_identical(ConvertToInchikey("C_???"), NA_character_)
+  expect_identical(ConvertToInchikey("C???"), NA_character_)
 })
 
 
